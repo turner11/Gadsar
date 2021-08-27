8f76789226f8fd24a2fb1f977491a28429444df0
Avi Turner <avi.turner111@gmail.com>
2021-08-27 12:35:49 +0300

    added the option to use google sheets (+ enhancments to legend selection)

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,42 @@
+from functools import reduce
+
 import pandas as pd
 import streamlit as st
 from pathlib import Path
 from utils import DataBundle
 import altair as alt
+import gsheetsdb
+
+# Create a connection object.
+conn = gsheetsdb.connect()
+
+
+@st.cache(ttl=10)
+def run_query(query) -> pd.DataFrame:
+    cursor = conn.execute(query, headers=1)
+    df = pd.DataFrame(cursor.fetchall())
+    return df.copy()
+
+
+def get_df():
+    sheet_url = st.secrets.get("public_gsheets_url", '')
+    query_params = st.experimental_get_query_params() or dict()
+    sheet_url = query_params.get('url', sheet_url)
+
+    path = sheet_url or Path('./data.xlsx').resolve()
+    excel_path = st.sidebar.text_input('Excel path / URL', str(path))
+
+    if excel_path.lower().startswith('http'):
+        path_arg = run_query(f'SELECT * FROM "{sheet_url}"')
+    else:
+        path_arg = path
+    df = DataBundle.get_base_data(path_arg)
+    return df
+
 
 def get_sidebar_inputs():
-    excel_path = st.sidebar.text_input('Excel path', Path('./data.xlsx').resolve())
+    df = get_df()
 
-    df = DataBundle.get_base_data(excel_path)
     plugot = list(df.pluga.drop_duplicates())
     plugot = st.sidebar.multiselect('סינון פלוגות', plugot, plugot)
     if plugot:
@@ -27,43 +56,46 @@ def main():
     with st.beta_expander('מידע גולמי', expanded=False):
         df
 
-
     with st.beta_expander('כמה הגיעו מכל פלוגה', expanded=False):
-        df_arrived_count = df_arrived.groupby('pluga').agg({'mi':'count'}).rename(columns={'mi':'count'}).sort_values('count')
+        df_arrived_count = df_arrived.groupby('pluga').agg({'mi': 'count'}).rename(columns={'mi': 'count'}).sort_values(
+            'count')
         df_arrived_count
         st.bar_chart(df_arrived_count)
 
     with st.beta_expander('כמה יש בכל קבוצה', expanded=True):
-        df_teams = df_arrived.groupby('group').agg({'mi':'count'}).rename(columns={'mi':'counts'}).sort_values('counts', ascending=False)
+        df_teams = df_arrived.groupby('group').agg({'mi': 'count'}).rename(columns={'mi': 'counts'}).sort_values(
+            'counts', ascending=False)
         df_teams
         st.bar_chart(df_teams)
 
     with st.beta_expander('זמני הגעה', expanded=True):
-        cols = ['pluga', 'hour', ] + [c for c in df_arrived.columns if 'name' in c]
+        cols = ['pluga', 'hour', 'team'] + [c for c in df_arrived.columns if 'name' in c]
         dfc = df_arrived[cols]
         dfc = dfc.sort_values('hour').reset_index()
-        # color_values = ['g','b','c','y', 'm','k', 'w']
-        # colors = dict(zip([p for p in dfc.pluga.drop_duplicates()], color_values))
-        # dfc['colors'] = dfc.pluga.apply(lambda p: colors[p])
-        dfc['total_arrived'] = pd.Series(range(len(dfc)), name='total_arrived') +1
+        dfc['total_arrived'] = pd.Series(range(len(dfc)), name='total_arrived') + 1
         dfc['time_stamp'] = dfc.hour.apply(lambda h: str(h.time()))
 
+        plugas = list(dfc.pluga.unique())
+        group_by = 'pluga' if len(plugas) != 1 else 'team'
+        dfc[group_by] = dfc[group_by].apply(lambda v: str(v))  # Make sure its categorical in legend
+        selection = alt.selection_multi(fields=[group_by], bind='legend')
+        charts = []
+        for group_name, dfp in dfc.groupby(group_by):
+            # size=50
+            chart_p = alt.Chart(dfp.reset_index()).mark_circle().encode(
+                x='hour',
+                y='total_arrived',
+                color=group_by,
+                tooltip=[c for c in cols if c in dfp.columns],
+                opacity=alt.condition(selection, alt.value(1), alt.value(0.2))
+            )
+            charts.append(chart_p)
+        if charts:
+            chart = reduce(lambda c1, c2: c1 + c2, charts)
+            chart = chart.add_selection(selection).interactive()
+            st_chart = st.empty()
+            st_chart.altair_chart(chart, use_container_width=True)
 
-        chart = st.empty()
-        x = alt.Chart(dfc).mark_circle().encode(
-            x='hour',
-            y='total_arrived',
-            color='pluga',
-            tooltip=list(sorted(dfc.columns)),
-        ).interactive()
-        chart.altair_chart(x, use_container_width=True)
-
-
-    # with st.beta_expander('זמני הגעה לפי מחלקות', expanded=True):
-    # if False:
-    #     from bokeh_script import get_bokeh_figure
-    #     lay = get_bokeh_figure(r'data.xlsx')
-    #     st.bokeh_chart(lay)
 
 
 if __name__ == '__main__':
